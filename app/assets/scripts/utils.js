03d0893bbce028b8e8b9ed39e7641b7cc079b691
sawojcicki <sawojcicki@gmail.com>
2017-07-18 10:41:52 -0400

    Update utils.js

--- app/assets/scripts/utils.js
+++ app/assets/scripts/utils.js
@@ -641,7 +641,7 @@ var utils = {
     var model = 'run';
     // If we don't have a match, return default
     if (ri === -1 || hi === -1) {
-      model += ('0' + li + '0');
+      model += ('0' + li + hi);
     } else {
       model = model + ri + li + hi;
     }
