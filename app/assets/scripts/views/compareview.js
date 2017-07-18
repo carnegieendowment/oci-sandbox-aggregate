ed22e1735f1a33602c4386acb7f573d84ff7d67a
sawojcicki <sawojcicki@gmail.com>
2017-07-18 14:51:45 -0400

    Update compareview.js

--- app/assets/scripts/views/compareview.js
+++ app/assets/scripts/views/compareview.js
@@ -296,7 +296,7 @@ var CompareView = BaseView.extend({
 
     // if we don't have the necessary data, load it
     var opgeeRun = utils.getOPGEEModel(params.solarSteam, params.water, params.flaring);
-    var prelimRun = utils.getPRELIMModel(params.refinery, params.lpg, params.hydrogen);
+    var prelimRun = utils.getPRELIMModel(params.hydrogen, params.refinery, params.lpg);
     if (!Oci.Collections.opgee.get(opgeeRun)) {
       var opgeeModel = new OpgeeModel({ id: opgeeRun });
       opgeeModel.fetch({ async: false, success: function (data) {
